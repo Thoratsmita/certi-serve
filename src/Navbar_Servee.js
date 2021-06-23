cbc16016805d4cf5810588113adef3fb822c0b7b
Parth Chaturvedi <pchaturvedi@my.bcit.ca>
2021-06-23 21:04:26 +0530

    build components

---
+++ src/Navbar_Servee.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import $ from 'jquery';
+
+export default function Navbar_Servee() {
+
+    useEffect(()=>{        
+        $('.nav-link').on('click', function() {
+            $('.active-link').removeClass('active-link');
+            $(this).addClass('active-link');
+        });
+    })
+
+    return (
+        <div className="Navbar">
+            <div className="leftside">
+                <div className="links">
+                    <a className="nav-link active-link" href="#myproject">My Project</a>
+                    <a className="nav-link" href="#dashboard">Dashboard</a>
+                    <a className="nav-link" href="#inbox">Inbox</a>
+                    <a className="nav-link" href="#feedback">Feedback</a>
+                    <a className="nav-link" href="#credit">Free Credit</a>
+                </div>
+            </div>
+            <div className="rightside"></div>
+        </div>
+    )
+}
---
+++ src/Projects_Servee.js
@@ -0,0 +1,54 @@
+import React, {useEffect} from 'react';
+import Navbar from './Navbar_Servee';
+import $ from 'jquery';
+
+export default function Projects_Servee() {
+
+    useEffect(()=>{        
+        $('.tab').on('click', function() {
+            $('.active-tab').removeClass('active-tab');
+            $(this).addClass('active-tab');
+        });
+    })
+
+    return (
+        <>
+            <Navbar />
+            <div className="PrjServee">
+                <div className="PrjServeeHead">
+                    <h1>Projects</h1>
+                    <button>Servee</button>
+                </div>
+                <div className="PrjServeeTab">
+                    <a className="tab active-tab" href="#openproject">Open Project</a>
+                    <a className="tab " href="#workinprogress">Work in Progress</a>
+                    <a className="tab " href="#pastproject">Past Project</a>
+                </div>
+                <form className="PrjServeeForm">
+                    <input type="text" name="search" placeholder="Search Project"/>
+                    <label id="show" htmlFor="show">Show</label>
+                    <select id="show">
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="40">30</option>
+                        <option value="100">100</option>
+                    </select>
+                    <label htmlFor="view">View</label>
+                    <select id="view">
+                        <option value="all">All</option>
+                        <option value="lorem">lorem</option>
+                        <option value="lorem">lorem</option>
+                        <option value="lorem">lorem</option>
+                        <option value="lorem">lorem</option>                        
+                    </select>
+                </form>
+                <div className="PrjServeeResult">
+                    <h3>Create a New Project</h3>
+                    <p>Millions of talented Servers are ready to help you do amazing things</p>
+                    <button>Post a Project</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
