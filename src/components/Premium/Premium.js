29cc5e278b8063cd78132240dbdc7be1659ce1a5
vivek binjola <vivekbinjola23@gmail.com>
2021-07-29 20:18:07 +0530

    built Premium Page

--- src/components/Premium/Premium.js
+++ src/components/Premium/Premium.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import './premium.css';
 import { Link } from "react-router-dom";
+import sub from "../../images/sub_icon.png";
+import logo from "../../images/certi-serv-logo.png";
+
 const Premium = () => {
     return (
-        <>
-        <div className="containerPremium">
-
-                <div className="premiumChoose" >
-                    <p>Choose Membership</p>
-                </div>
-                
-                <div className="mainPremium">
-                    <Link to="/" className="premiumBasic">
-                        <div className="premiumP">
-                            <p className="premiumP1">Basic</p>
-                            <p className="premiumP2">Free</p>
-                         </div>
-                    </Link>   
-                </div>
-
-
-
+     <>
+     <div className="containerPremium">
+        <div className="containerNavPremium">
+            <img src={logo} className="logoPremium" alt="logo" />
         </div>
             
+        <div className="mainPremiumContent">
+            <div className="chooseMembership">
+                Choose Membership
+            </div>
+            <div className="boxPremium">
+                 <Link to="/" className="buttonTopPremium"> 
+                  <div className="basicPremium">
+                      <a>Basic</a>
+                    </div>
+                    <div className="freePremium">
+                      <a>Free</a>
+                    </div>
+                 </Link>
+
+                <Link to="/" className="buttonBottomPremium"> 
+                    <div className="bottomPremium">
+                      <a>Premium</a>
+                    </div>
+                    <div>
+                      <img src={sub} className="imagePremiumBottom" alt="image"/>
+                    </div>
+                </Link>
+               
+                <Link to="/" className="skipPremium">
+                    <p >Skip for now ---></p>
+                </Link>
+
+            </div>
+        </div>
+       
+          </div>  
         </>
     )
 }
